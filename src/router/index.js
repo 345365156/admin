2cab0c924f44017bc15e2652cd65ae21a4b32e21
345365156@qq.com <LiSen243364868>
2019-08-16 00:40:52 +0800

    git test

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,93 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../views/Login'
 import Layout from '../views/Layout'
 
 Vue.use(Router)
 
+/**
+ * 备注: sub-menu 只会当 children.length >= 1 时出现
+ * 详情 见: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   如果设为 true, item 不会在 sidebar 组件中显示(默认为 false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
+
+/**
+ * constantRoutes
+ * 不需要权限的基本页面
+ * 所有角色都被授权访问
+ */
 export const constantRoutes = [
   {
     path: '/login',
-    name: 'login',
-    component: Login
+    component: () => import('@/views/Login'),
+    hidden: true
   },
+
   {
     path: '/404',
-    name: '404',
-    component: () => import('@/views/404')
+    component: () => import('@/views/404'),
+    hidden: true
   },
+
   {
     path: '/',
     component: Layout,
-    redirect: 'dashboard',
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example' },
     children: [
       {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard' }
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
       }
     ]
-  }
+  },
+
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () => new Router({
